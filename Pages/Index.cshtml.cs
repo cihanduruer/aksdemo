ef961cb1e2d3c54cc759130046c3c698339b6ae9
Cihan Duruer <cihan.duruer@gmail.com>
2024-10-11 13:03:23 +0200

    added hostname

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,10 +6,12 @@ namespace DemoContainerApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public string Hostname { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            Hostname = Environment.MachineName;
         }
 
         public void OnGet()
